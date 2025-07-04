d7b260944692004260dfebec05ec35cf48903bb5
Carlos Martorell <cmartorellotal@gmail.com>
2025-07-04 13:02:11 +0200

    Add Manager section layout with responsive styles

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  content: ['./src/**/*.html'], // Rutas donde Tailwind buscará clases
+  content: ['./index.html','./src/**/*.html'], // Rutas donde Tailwind buscará clases
   theme: {
     extend: {
       colors: {
         customRed: '#FF5F57', // Definimos el color coral
+        customBlue: '#4d5bed',
       },
     },
   },
