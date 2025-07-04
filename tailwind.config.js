5b5d49c61611357ce3a86b04d8c46fb46965a2df
Carlos Martorell <cmartorellotal@gmail.com>
2025-07-04 11:12:18 +0200

    Configure Tailwind CSS and update header styles

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.html'], // Rutas donde Tailwind buscará clases
+  theme: {
+    extend: {
+      colors: {
+        customRed: '#FF5F57', // Definimos el color coral
+      },
+    },
+  },
+  plugins: [],
+};
